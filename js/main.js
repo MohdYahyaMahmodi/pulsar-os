890fa61bdf7f0d39ccacbe1664226493c197d905
NINJACLUBREAL <168763162+NINJACLUBREAL@users.noreply.github.com>
2024-08-17 13:00:35 -0400

    s

--- js/main.js
+++ js/main.js
@@ -9,7 +9,6 @@ const windows = {
   'clock-window': { icon: '⏰', title: 'Clock & Alarm' },
   'video-window': { icon: '🎥', title: 'Video Player' },
   'terminal-window': { icon: '💻', title: 'Terminal' }
-  'whats-new-window': { icon: '❔', title: 'Whats New'}
 };
 
 let highestZIndex = 100;
