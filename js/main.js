e92ac94176afb6a7e0498f666253cfc8c55f4270
NINJACLUBREAL <168763162+NINJACLUBREAL@users.noreply.github.com>
2024-08-17 12:27:43 -0400

    bghjhj

--- js/main.js
+++ js/main.js
@@ -363,3 +363,17 @@ scripts.forEach(script => {
     scriptElement.src = script;
     document.body.appendChild(scriptElement);
 });
+
+document.getElementById('openPopupBtn').addEventListener('click', function() {
+  document.getElementById('popup').style.display = 'flex';
+});
+
+document.querySelector('.close-btn').addEventListener('click', function() {
+  document.getElementById('popup').style.display = 'none';
+});
+
+window.addEventListener('click', function(event) {
+  if (event.target === document.getElementById('popup')) {
+      document.getElementById('popup').style.display = 'none';
+  }
+});
